aada6fabcade81d699b56f94d96f20ffae3aff2e
Victor Chen <victorchen945@163.com>
2017-04-14 16:11:17 +0800

    Update and rename nested function option.py to class/nested function option.py

--- class/nested function option.py
+++ class/nested function option.py
@@ -15,33 +15,38 @@ civ.calc()   #<------call function calc()
 #it will print ->"civil engineer gives you an ansys result"
 
 #also i can write in this way:
-class civileng2:
+class civilengineer2:
     def __init__(self,age2):
         self.age=age2
         def calc(self):
             def ansys(): ###we do not need 'self' inside
                 print "civil engineer gives you an ansys result"
             return ansys #<-----we can also return the 'tag' of inner func to call inner func
-civ=civileng2(age2)
+civ=civilengineer2(age2)
 civ.calc()()  #<------attention! we should write two quotes here
 #also print ->"civil engineer gives you an ansys result"
 
-######wrong way!!! #######
-class civileng3:
+######  wrong way!!! #######
+class civilengineer3:
     def __init__(self,age2):
         self.age=age2
-        def calc(self):
-            def ansys():
-                print "civil engineer gives you an ansys result"
-            return OK #<----return a int without calling inner func
-civ=civileng3(age2)
+    def callforhelp():
+        myArchitect=architect(age1)
+        myArchitect.draw.ps()
+        print "architect provides you a rendering"
+    def calc(self):
+        def ansys():
+            print "civil engineer gives you an ansys result"
+        return OK #<----return a int without calling inner func
+civ=civilengineer3(age2)
 civ.calc()()  #<------then it is impossible to call inner func\
 
-              #that's because civileng3 is infact an architect and he knows nothing about structure\
-              #he can not use ANSYS but he just pretend that he can\
-              #he said 'OK' to you, but before the ddl he will deceive you that something is wrong...
+              #--|| that's because civilengineer3 is infact an architect\
+              #     he knew nothing about structure\
+              #     he can not use ANSYS but just pretended that he can\
+              #     he said 'OK' to you, but before the ddl he lied that his burnt his computer...
 
-              #system will report error: "int is not callable"
+              
 
 
 
